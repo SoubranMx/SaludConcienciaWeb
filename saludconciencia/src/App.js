e61caba2ec449502ad195345251af937639a259a
Uriel Soubran <uriel_bee15@hotmail.com>
2021-07-27 03:23:00 -0500

    Inicial

---
+++ saludconciencia/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+//import './css/style.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="container">
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
