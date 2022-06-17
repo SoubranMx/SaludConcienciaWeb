73becede20e28d32e965ed1cc0034ccc3f57d19b
Uriel Soubran <uriel_bee15@hotmail.com>
2022-06-17 11:00:04 -0500

    QualityLife update on Firebase.js
    En vez de estar comentando como un pelotudo la llave del test o deploy, los cambie a dos variables y solo tengo que cambiar dicha variable.

--- saludconciencia/src/firebase.js
+++ saludconciencia/src/firebase.js
@@ -9,15 +9,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 import 'firebase/storage'
 
-const firebaseConfig = {
-    // Test
-    // apiKey: "AIzaSyAWR00NslWFatdQ4sF3BF2uG_HL6bBCv7c",
-    // authDomain: "prueba-editorjs.firebaseapp.com",
-    // projectId: "prueba-editorjs",
-    // storageBucket: "prueba-editorjs.appspot.com",
-    // messagingSenderId: "930841760288",
-    // appId: "1:930841760288:web:7c85c1b521381e0e422245"
-    
+const firebaseConfigDeploy = {
     //Deploy
     apiKey: "AIzaSyC2fCz_gNhMUO2mDwT--04KgBFIThLCTQI",
     authDomain: "saludconciencia-53c0a.firebaseapp.com",
@@ -26,6 +18,19 @@ const firebaseConfig = {
     messagingSenderId: "759473753418",
     appId: "1:759473753418:web:f926117bbc1a52828f45b3"
 };
+
+const firebaseConfigTest = {
+    // Test
+    apiKey: "AIzaSyAWR00NslWFatdQ4sF3BF2uG_HL6bBCv7c",
+    authDomain: "prueba-editorjs.firebaseapp.com",
+    projectId: "prueba-editorjs",
+    storageBucket: "prueba-editorjs.appspot.com",
+    messagingSenderId: "930841760288",
+    appId: "1:930841760288:web:7c85c1b521381e0e422245"
+}
+
+const firebaseConfig = firebaseConfigTest;
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
